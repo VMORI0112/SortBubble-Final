d4665e76a169d587f4b69bd08149a40ec5ffbac7
samir benzada <samirbenzada@gmail.com>
2019-10-01 17:07:02 -0400

    samir pushed

--- js/index.js
+++ js/index.js
@@ -1,44 +1,26 @@
 // Here your functions Victor
 let suit = ["club","diamond","spade","heart"];
-var cardSortDet = [];
-cardtimes=0;
 
-cardTimes = document.querySelector("#row").innerHTML.value
-
-
-const cards = () => {  
-  for (let i=0; i < cardtimes; i++) {
-      let colorC = getcolor() ;
-      let suitC = getSuites() ;
-      let cardC = getCard() ;
-
-      document.querySelector("#cards").innerHTML= `${colorC} ${suitC} ${csuitC}` ;
-
-  }
-
-
-}
-
-const getColor = () => {
+function howManyCards() {
+  document.body.querySelector('#cards').innerHTML ="";
+  let InputVal = document.body.querySelector('#inputNumber').value;
+  for (let i=0; i<InputVal;i++) {
     let colorCardNumber = Math.floor(Math.random() * 2) + 1;
-    let colorCard;
-     switch(colorCardNumber) {
-     case 1:
-       colorCard="red"
-       break;
-     case 2:
-        colorCard="black"
-       break;
-     }  
-    return colorCard;
-}
-
-const getSuites = () => {
+    switch(colorCardNumber) {
+    case 1:
+      colorCard="red"
+      break;
+    case 2:
+       colorCard="black"
+      break;
+    }  
     let random = Math.floor(Math.random() * suit.length);
-    return suit[random];
+    let randomCard = Math.floor(Math.random() * 14) +1;
+
+    document.body.querySelector('#cards').innerHTML += `<li>color: ${colorCard} || suit: ${suit[random]} || Cart Number: ${randomCard}</li>`;
+  }
 }
 
-const getCard = () => {
-    let randomCard = Math.floor(Math.random() * 14) +1;
-    return randomCard
-}
+  
+  
+
