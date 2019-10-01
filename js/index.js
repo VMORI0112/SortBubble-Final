b0f9a4298d07bb51fcbabe7840f5f008a28a39d9
samir benzada <samirbenzada@gmail.com>
2019-10-01 15:29:56 -0400

    some functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Here your functions Victor
+let suit = ["club","diamond","spade","heart"];
+var cardSortDet = [];
+let cardNumber="0";
+
+function getColor() {
+    let colorCardNumber = Math.floor(Math.random() * 2) + 1;
+    let colorCard;
+     switch(colorCardNumber) {
+     case 1:
+       colorCard="red"
+       break;
+     case 2:
+        colorCard="black"
+       break;
+     }  
+    return colorCard;
+}
+
+const getSuites = () => {
+    let random = Math.floor(Math.random() * suit.length);
+    return suit[random];
+}
+
+const getCard = () => {
+    let randomCard = Math.floor(Math.random() * 14) +1;
+    return randomCard
+}
