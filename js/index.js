aa94f56c7bb935569767d198ac9a1af5e2d41da6
samir benzada <samirbenzada@gmail.com>
2019-10-02 10:45:00 -0400

    Samir Done

--- js/index.js
+++ js/index.js
@@ -18,9 +18,11 @@ const howManyCards = () => {
     }  
     let random = Math.floor(Math.random() * suit.length);
     let randomCard = Math.floor(Math.random() * 14) +1;
-    cardArr.push(randomCard);
+
+    cardArr.push({"suit": suit[random], "color": colorCard, "number": randomCard});
     document.body.querySelector('#cards').innerHTML += `<div class="PlayingCard mt-3"><span class="topLeft${suit[random]} ${colorCard}"></span>  ${randomCard} <span class="bottomRight${suit[random]} ${colorCard}"></span></div>`;
   }
+
   return cardArr;
 }
 
@@ -29,25 +31,28 @@ log = [];
 
 function sortBubble() {
   document.body.querySelector('#cardsSortDetail').innerHTML = '';
-  var len = cardArr.length,i, j, stop;
 
-  for (i=0; i < len; i++){
-    for (j=0, stop=len-i; j < stop; j++){
-        if (cardArr[j] > cardArr[j+1]){
-            var temp = cardArr[j];
-            cardArr[j] = cardArr[j+1];
-            cardArr[j+1] = temp;
-            log.push(cardArr.slice(0));
+  var swapped;
+    do {
+        swapped = false;
+        for (var i = 0; i < cardArr.length - 1; i++) {
+            if (cardArr[i].number > cardArr[i + 1].number) {
+                var temp = cardArr[i];
+                cardArr[i] = cardArr[i + 1];
+                cardArr[i + 1] = temp;
+                swapped = true;
+                log.push(cardArr.slice(0));
+            }
         }
-    }
-  }
+    } while (swapped);
+
 
   for (num in cardArr) {
-    document.body.querySelector('#cardsSortDetail').innerHTML += `<div class="PlayingCard mt-3">${cardArr[num]}</div>`;
+    document.body.querySelector('#cardsSortDetail').innerHTML += `<div class="PlayingCard mt-3"><span class="topLeft${cardArr[num].suit} ${cardArr[num].color}"></span> ${cardArr[num].number} <span class="bottomRight${cardArr[num].suit} ${cardArr[num].color}"></span></div>`;
   }
   for (swap in log) {
     for (card in log[swap]) {
-      document.body.querySelector('#detail').innerHTML += `<div class="PlayingCard mt-3">${log[swap][card]}</div>`;
+      document.body.querySelector('#detail').innerHTML += `<div class="PlayingCard mt-3"><span class="topLeft${log[swap][card].suit} ${log[swap][card].color}"></span> ${log[swap][card].number} <span class="bottomRight${log[swap][card].suit} ${log[swap][card].color}"></span></div>`;
     }
   }
 }
